93659f3d5cd9dbb25639dd2ac0807ed9d48f1814
melosomelo <44986311+melosomelo@users.noreply.github.com>
2019-05-22 12:46:33 -0300

    Final changes in the actual game src & include

---
+++ src/ioput.cpp
@@ -0,0 +1,206 @@
+#include "../include/ioput.h"
+
+
+
+Life ioput::read_input()
+{
+    int height, lenght; //dimensions of the cell grid
+    std::ifstream input_specs; //file from which we will read the specs
+    std::string line; //string to store lines from the file 
+    char alive_char;
+    input_specs.open(this->infile);
+    input_specs >> height >> lenght;
+    Life grid(height, lenght); //initializing the new object
+    input_specs >> alive_char; 
+    input_specs.get();
+    input_specs.get();
+    for(int i = 0 ; i < height; i++)
+    {
+        std::getline(input_specs, line);
+        for(int j = 0; j < lenght; j++)
+        {
+            if(line[j] == alive_char)
+            {
+                grid.Cells[i+1][j+1].alive = true;
+                grid.current.living_cells.push_back(grid.Cells[i+1][j+1]);
+            }
+        }
+        
+    }
+    grid.current.generation_number = 0;
+    grid.current.state = Running;
+    return grid;
+
+
+}
+
+life::Color ioput::determine_color(std::string color)
+{
+    if(color == "BLACK")
+        return life::BLACK;
+    else if(color == "BLUE")
+        return life::DEEP_SKY_BLUE;
+    else if(color == "CRIMSON")
+        return life::CRIMSON;
+    else if(color == "DARK_GREEN")
+        return life::DARK_GREEN;
+    else if(color == "DEEP_SKY_BLUE")
+        return life::DEEP_SKY_BLUE;
+    else if(color == "DODGER_BLUE")
+        return life::DODGER_BLUE;
+    else if(color == "GREEN")
+        return life::GREEN;
+    else if(color == "LIGHT_BLUE")
+        return life::LIGHT_BLUE;
+    else if(color == "LIGHT_GREY")
+        return life::LIGHT_GREY;
+    else if(color == "LIGHT YELLOW")
+        return life::LIGHT_YELLOW;
+    else if(color == "RED")
+        return life::RED;
+    else if(color == "STEEL_BLUE")
+        return life::STEEL_BLUE;
+    else if(color == "WHITE")
+        return life::WHITE;
+    else if(color == "YELLOW")
+        return life::YELLOW;
+    else
+    {
+        std::cout << ">>>>COLOR NOT FOUND. SETTING COLOR TO DEFAULT...\n";
+        return this->bkgcolor;
+    }
+    
+}
+
+void ioput::check_cmd_line_specs(char* argv[], int argc)
+{
+    bool skip = false;
+    for(int i = 1; i < argc; i++)
+    {
+        std::string str_test = argv[i];
+        if(skip)
+        {
+            skip = false;
+            continue;
+        }
+        if(str_test == "--help")
+        {
+            this->help = true;
+        }
+        else if(str_test=="--imgdir")
+        {
+            this->imgdir = argv[i+1];
+            skip = true;
+        }
+        else if(str_test=="--maxgen")
+        {
+            this->maxgen = std::atoi(argv[i+1]);
+            skip = true;
+
+        }
+        else if(str_test == "--fps")
+        {
+            this->fps = std::atoi(argv[i+1]);
+            skip = true;
+
+        }
+        else if(str_test == "--blocksize")
+        {
+            this->blocksize = std::atoi(argv[i+1]);
+            skip = true;
+        }
+        else if(str_test == "--bkgcolor")
+        {
+            std::string color = argv[i+1];
+            this->bkgcolor = determine_color(color);
+            skip = true;
+        }
+        else if(str_test == "--alivecolor")
+        {
+            std::string color = argv[i+1];
+            this->alivecolor = determine_color(color);
+            skip = true;
+        }
+        else if(str_test == "--outfile")
+        {
+            this->outfile = argv[i+1];
+            skip = true;
+        }
+        else
+        {
+            this->infile = argv[i];
+        }
+        
+        
+    }
+
+    
+}
+
+void encode_png(const char* filename, const unsigned char * image, unsigned width, unsigned height)
+{
+    //Encode the image
+    unsigned error = lodepng::encode(filename, image, width, height);
+
+    //if there's an error, display it
+    if(error) std::cout << "encoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
+}
+
+void ioput::paint_canvas(life::Canvas &image, Life &game, int counter)
+{
+
+    life::Color alive_color = this->alivecolor;
+    life::Color bkg = this->bkgcolor;
+    std::string temporary = this->imgdir;
+    temporary += "Generation";
+    temporary += std::to_string(counter);
+    temporary += ".png";
+    const char* filename = temporary.c_str();
+    for(auto x (0u); x < game.get_lenght(); x++)
+    {
+        for(auto y (0u); y < game.get_height(); y++)
+        {
+            if(game.Cells[y+1][x+1].alive)
+                image.pixel(life::Point2(x,y), this->alivecolor);
+            else
+                image.pixel(life::Point2(x,y), this->bkgcolor);
+
+        }
+    }
+    encode_png(filename, image.pixels(), image.width(), image.height());
+
+}
+
+void ioput::print() //this is a test function to test if the specs are being captured. It will be deleted later.
+{
+    std::cout << "HELP IS " << this->help << std::endl
+              << "IMGDIR IS " << this->imgdir << std::endl
+              << " MAXGEN IS " << this->maxgen << std::endl
+              << " FPS IS" << this->fps << std::endl
+              << " BLOCKSIZE IS " << this->blocksize << std::endl
+              << " OUTFILE IS " << this->outfile << std::endl;
+
+}
+
+
+
+void print_help()
+{
+    std::cout << "Usage: glife [<options>] <input_cfg_file> \n"
+              << "  Simulation options: \n"
+              << "      --help Print this help text.\n"
+              <<  "     --imgdir <path> Specify directory where output images are written to. \n"
+              <<  "     --maxgen <num> Maximum number of generations to simulate.\n"
+              <<  "     --fps <num> Number of generations presented per second.\n"
+              <<  "     --blocksize <num> Pixel size of a cell. Default = 5.\n"
+              <<  "     --bkgcolor <color> Color name for the background. Default GREEN.\n"
+              <<  "     --alivecolor <color> Color name for representing alive cells. Default RED.\n"
+              <<  "     --outfile <filename> Write the text representation of the simulation\n"
+              <<  "     to the given filename.\n"
+              << "  Available colors are:\n"
+              <<"       BLACK BLUE CRIMSON DARK_GREEN DEEP_SKY_BLUE DODGER_BLUE \n"
+              << "      GREEN LIGHT_BLUE LIGHT_GREY LIGHT_YELLOW RED STEEL_BLUE \n"
+              <<"       WHITE YELLOW \n";
+
+}
+
---
+++ src/life.cpp
@@ -0,0 +1,166 @@
+#include "../include/life.h"
+
+
+Life::Life(int lines, int columns)
+{
+    Cell dead(false);
+    Grid temp_grid (lines+2, std::vector<Cell>(columns+2));
+    for(int i = 0; i < lines+2; i++)
+    {
+        for(int j = 0; j < columns+2; j++)
+        {
+            temp_grid[i][j] = dead;
+            temp_grid[i][j].line = i;
+            temp_grid[i][j].column = j;
+        }
+    }
+    this->Cells = temp_grid;
+    this->lenght = columns;
+    this->height = lines;
+    this->current.state = Running;
+    this->current.generation_number = 0;
+}
+
+int Life::count_alive_neighbours(Cell &subject) const
+{
+    int alive_neighbours = 0;
+    for(auto i = subject.line - 1; i <= subject.line + 1; i++)
+    {
+        for(auto j = subject.column - 1; j <=subject.column + 1; j++ )
+        {
+            if(i == subject.line && j == subject.column)   
+                continue;
+            else
+            {
+                if(this->Cells[i][j].alive)
+                    alive_neighbours++;
+                
+            }
+        }
+    }
+    return alive_neighbours;
+}
+bool Life::Rule01(Cell &subject) const
+{
+    int alive_neighbours = count_alive_neighbours(subject);
+    return alive_neighbours > 1;
+
+}
+
+
+bool Life::Rule02(Cell &subject) const
+{
+    int alive_neighbours = count_alive_neighbours(subject);
+    return alive_neighbours < 4;
+
+}
+
+bool Life::Rule03(Cell &subject) const
+{
+    int alive_neighbours = count_alive_neighbours(subject);
+    return (alive_neighbours == 2) || (alive_neighbours == 3);
+}
+
+bool Life::Rule04(Cell &subject) const
+{
+    int alive_neighbours = count_alive_neighbours(subject);
+    return alive_neighbours == 3;
+}
+
+void Life::check_cell_state(Cell &subject)
+{
+    if(subject.alive)
+        subject.alive_next_state = (Rule01(subject) && Rule02(subject) && Rule03(subject));
+    else
+        subject.alive_next_state = Rule04(subject);
+}
+
+void Life::check_surroundings(Cell &subject)
+{
+    for(int i = subject.line - 1; i <= subject.line + 1; i++)
+    {
+        for(int j = subject.column - 1; j <=subject.column + 1; j++)
+        {
+            if(i == 0 || j == 0 || i == this->height +1 || j == this->lenght +1) // we skip the ones in the outer limits
+            {   
+                continue;
+            }
+            this->check_cell_state(this->Cells[i][j]);
+        }
+    }
+}
+
+void Life::update_current_generation()
+{
+    for(int i = 0; i < this->height; i++) //updating the state of the cells
+    {
+        for(int j = 0; j < this->lenght; j++)
+        {
+            this->Cells[i+1][j+1].alive = this->Cells[i+1][j+1].alive_next_state; //updating the state of each single cell
+            this->Cells[i+1][j+1].alive_next_state = false;
+        }
+    }
+    this->all_generations.push_back(this->current); //storing the current generation
+    this->current.living_cells.clear(); //clearing the living cells vector so that we can add the new ones
+    Generation temp = Generation(this->Cells, this->height, this->lenght); //Creating a temporary generation 
+    this->current = temp; //assigning the current generation to the temporary one we created
+    this->current.generation_number += 1;
+
+    
+}
+
+int Life::search_for_equal_gen() 
+{
+    for(const auto &i: this->all_generations)
+    {   
+        if(i == this->current)
+        {
+            this->current.state = Stable;
+            return i.generation_number;
+        } 
+        
+    }
+    return -1;
+}
+
+void Life::update_gen_status()
+{
+    if(this->current.state == Stable || this->current.state == Extinct)
+    {
+        this->current.state = Game_Over;
+        std::cout << ">>>>GAME OVER<<<< \n";
+    }
+    else
+    {
+        int possible_equal = this->search_for_equal_gen();
+        if(possible_equal != -1)
+        {
+            std::cout << ">>>FOUND THAT GENERATION " << possible_equal << " IS EQUAL TO THE CURRENT ONE. SIMULATION IS NOW STABLE. \n";
+            this->current.state = Stable;
+        }
+        else if(this->current.living_cells.size() == 0)
+        {
+            this->current.state = Extinct;
+            std::cout << ">>>>ALL CELLS ARE DEAD. GENERATION IS EXTINTCT. \n";
+        }
+        else
+        {
+            this->current.state = Running;
+        }
+        
+
+    }
+    
+
+}
+
+void Life::update()
+{
+    for(auto &i: this->current.living_cells)
+        this->check_surroundings(i);
+    this->update_current_generation();
+    this->update_gen_status();
+
+}
+
+
